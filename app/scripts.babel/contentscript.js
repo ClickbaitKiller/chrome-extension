6d48eeea05925254ff48a3251509ed6a32fa9ae4
Pierre Gabioud <pierregabioud@gmail.com>
2017-11-11 14:05:55 +0100

    disappearing file

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -2,5 +2,33 @@
 
 console.log('Hello content script');
 
+let originHref = 'https://artmyn.com/partners/cultural';
 
-document.body.style.backgroundColor = 'red';
+
+let links = [...document.body.getElementsByTagName("a")];
+
+console.log(links);
+
+let cleanLinks = links.map((link, i) => {
+  return {
+    href: link.href,
+    text: link.innerText,
+    index: i
+  }
+}).filter(link => {
+  return link.href.indexOf('javascript:') === -1 && link.text && link.href !== originHref;
+});
+
+let imgLink = cleanLinks.find(link => {
+  return link.href.indexOf('imgur') !== -1;
+});
+
+console.log(cleanLinks);
+console.log(imgLink);
+
+
+$.get(chrome.extension.getURL('/html/clickbait-killer.html'), function(data) {
+  $(data).appendTo('body');
+  // Or if you're using jQuery 1.8+:
+  // $($.parseHTML(data)).appendTo('body');
+});
